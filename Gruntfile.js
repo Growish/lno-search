d78831988411a56d9701c0d2b224b9a0f7daad60
domingo <ing.domingo.sl@gmail.com>
2017-04-03 12:34:14 +0200

    Doc updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,20 @@ module.exports = function(grunt) {
 
 
     grunt.initConfig({
+        credentials: grunt.file.readJSON("credentials.json"),
+        s3: {
+            options: {
+                accessKeyId: "<%= credentials.aws.accessKeyId %>",
+                secretAccessKey: "<%= credentials.aws.secretAccessKey %>",
+                bucket: "growish-partner",
+                region: "eu-central-1"
+            },
+            move: {
+                cwd: "dist/",
+                src: "**",
+                dest: "widget-network/"
+            }
+        },
         cssmin: {
             target: {
                 files: {
@@ -26,13 +40,34 @@ module.exports = function(grunt) {
                 },
                 src: [ 'src/*.js']
             }
+        },
+        add_comment: {
+            app1: {
+                options: {
+                    comments: ['Compiled ' + new Date()],
+                    carriageReturn: "\n",
+                    prepend: true,
+                    syntaxes: {
+                        '.js': '//',
+                        '.css': ['/*', '*/']
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: ['**/*.js', '**/*.css'],
+                    dest: 'dist/'
+                }]
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-aws');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-stripcomments');
+    grunt.loadNpmTasks('grunt-add-comment');
 
-    grunt.registerTask('default', ['comments', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['comments', 'cssmin', 'uglify', 'add_comment', 's3']);
 
 };
